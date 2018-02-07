9a17a551e06f497aa8f014ef7d03b3c0257eca20
JavaSDragon <PaltersDrakon@yandex.ru>
2018-02-07 11:13:26 +0300

    question

--- main.js
+++ main.js
@@ -1,37 +1,69 @@
-//Создаём метод добавления задач и добавляем элементы для новых обработчиков.
-let addBtn=document.getElementById('addBtn');
-addBtn.addEventListener('click',()=>{
-    let li=document.createElement('li');
-    let inputValue=document.getElementById('myInput').value;
-    let text=document.createTextNode(inputValue);
-    li.appendChild(text);
-    if(inputValue===""){
-        alert('add you task')
-    }
-    else{
-        document.getElementById('myUl').appendChild(li);
-    }
-    document.getElementById('myInput').value="";
-    let spanCls = document.createElement("span");
-    let spanEdit = document.createElement("span");
-    let txtCls=document.createTextNode("\u00D7")
-    let txtEdit=document.createTextNode("edit")
-    spanCls.className="close";
-    spanEdit.className="edit";
-    spanCls.appendChild(txtEdit);
-    spanEdit.appendChild(txtCls);
-    li.appendChild(spanCls);
-    li.appendChild(spanEdit);
-});
+(function () {
 
-//Метод для зачёркивания,удаленя,изменения задачи.
-let list=document.querySelector('ul');
-list.addEventListener('click',(ev)=>{
-    if (ev.target.tagName==="LI"){
-        ev.target.classList.toggle('checked');
+    var save;
+
+    const addBtn = document.getElementById('addBtn');
+    const inputRef = document.getElementById('myInput');
+    const ulRef = document.getElementById('myUl');
+    //Метод добавления элементов
+    const createElem = ({ type, className = '', view }) => {
+        const elem = document.createElement(type);
+        const text = document.createTextNode(view);
+        elem.className = className;
+        elem.appendChild(text);
+        return elem;
     }
-    else if(ev.target.tagName==="span"){
-        let div=ev.target.parentNode;
-        div.remove();
+    //Создаём метод добавления задач и добавляем элементы для новых обработчиков.
+    addBtn.addEventListener('click', () => {
+        const inputValue = inputRef.value;
+        const li = createElem({ type: 'li', className: '', view: inputValue });
+
+        if (inputValue === "") {
+            alert('add you task');
+        } else {
+            ulRef.appendChild(li);
+        }
+
+        inputRef.value = "";
+        li.appendChild(createElem({ type: 'span', className: 'edit', view: "edit" }));
+        li.appendChild(createElem({ type: 'span', className: 'close', view: "\u00D7" }));
+        li.appendChild(createElem({ type: 'input', className: 'hide' })).value = inputValue;
+        saveTask();
+    });
+    //Метод для зачёркивания,удаленя,изменения задачи.
+    const list = document.querySelector('ul');
+    list.addEventListener('click', ({ target: { tagName, classList, className, parentNode } }) => {
+        if (tagName.toLowerCase() === "li") {
+            classList.toggle('checked');
+            return;
+        }
+
+        if (className === "close") {
+            parentNode.remove();
+        }
+        if (className === "edit") {
+            const inputTask=parentNode.lastChild;
+           // console.log(inputTask.value);
+           /* inputTask.addEventListener("keypress",(event)=>{
+                event.preventDefault();
+                let result=inputTask.value
+                if (event.KeyCode == 13){
+                    parentNode.firstChild.textContent = result;
+                }
+            })*/
+            /*let result = prompt('Update you task', '');
+            parentNode.firstChild.textContent = result;*/
+        }
+       
+    
+        saveTask();
+    }, false);
+    //Метод сохранения задач в localStorage.
+    const saveTask = () => {
+        save = document.getElementById('myUl').innerHTML;
+        localStorage.setItem("save", JSON.stringify(save));
     }
-},false);
+    //Загрузка заданий из localStorage .
+    save = JSON.parse(localStorage.getItem("save"));
+    document.getElementById('myUl').innerHTML = save;
+})()
